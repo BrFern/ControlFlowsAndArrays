d2a16dcdf082178134755c856f3f668ba68dd395
BrFern <102929013+BrFern@users.noreply.github.com>
2022-10-12 14:53:04 -0600

    added if/else statements exercise

---
+++ exercises/ExerciseIfElseStatment.js
@@ -0,0 +1,9 @@
+var (num = "");
+
+if (num > 18) {
+    return "Good job being old";
+}
+
+else (num < 18 ) {
+    return "Get outta here youngin'!"
+}
---
+++ exercises/app.js
@@ -0,0 +1,13 @@
+const age= 18; {
+    if (num => 18) {
+        console.log ("Good job being old");
+    }
+    
+    else (num <= 18 ) {
+        console.log ("Get outta here youngin'!");
+    }
+}
+
+var num;
+
+
