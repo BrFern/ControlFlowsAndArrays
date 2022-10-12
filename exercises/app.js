0bb3dfc961e1777c4397ee18ec18925a9f52337e
BrFern <102929013+BrFern@users.noreply.github.com>
2022-10-12 15:55:34 -0600

    letter grade exercise

--- exercises/app.js
+++ exercises/app.js
@@ -1,13 +1,37 @@
-const age= 18; {
+/*const age= 18; {
     if (num => 18) {
         console.log ("Good job being old");
     }
     
-    else (num <= 18 ) {
+    else (num <= 18 ) 
         console.log ("Get outta here youngin'!");
-    }
+    
 }
 
 var num;
 
+var num;
+   if (num) {
+    if (num >= 100)
+      console.log ("Your number is positive and greater than 100!");
+    else if (num <= 99)
+      console.log ("Your number is positive and less than 100!");
+    else if (num < 0)
+       console.log ("Your number is negative.");
+   }
+*/
 
+
+let numberGrade= 67;{
+    if (numberGrade >= 90) {
+        console.log ("You got an A.");
+    } else if (numberGrade >= 80) {
+         console.log ("You got a B.");
+    } else if (numberGrade >= 70){
+        console.log ("You got a C");
+    } else if (numberGrade >= 60){
+         console.log ("You got a D");
+    } else if (numberGrade <= 50)
+        console.log ("You got an F");
+    }
+    
